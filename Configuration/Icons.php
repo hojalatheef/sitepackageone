d46f1657463665e93f6f8901a4a3aa4dc3de881a
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2024-08-07 17:54:18 +0200

    [TASK] Added new page type section for the onepage integration

---
+++ Configuration/Icons.php
@@ -0,0 +1,10 @@
+<?php
+
+use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
+
+return [
+    'tx-sitepackageone-page-section' => [
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:sitepackageone/Resources/Public/Icons/page-section.svg'
+    ],
+];
--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -6,43 +6,63 @@ if (!defined('TYPO3')) {
 
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
-ExtensionManagementUtility::registerPageTSConfigFile(
-    'sitepackageone',
-    'Configuration/TsConfig/Page/config.tsconfig',
-    'SitepackageOne Page TsConfig'
-);
+(static function () {
+    ExtensionManagementUtility::registerPageTSConfigFile(
+        'sitepackageone',
+        'Configuration/TsConfig/Page/config.tsconfig',
+        'SitepackageOne Page TsConfig'
+    );
 
-$newPageColumns = array(
-    'style_id' => array(
-        'exclude' => 0,
-        'label' => 'LLL:EXT:sitepackageone/Resources/Private/Language/locallang.xlf:style_id',
-        'config' => array(
-            'type' => 'input',
-            'size' => 30,
-            'eval' => 'nospace,alphanum_x,lower,unique'
-        ),
-    ),
-    'style_class' => array(
-        'exclude' => 0,
-        'label' => 'LLL:EXT:sitepackageone/Resources/Private/Language/locallang.xlf:style_class',
-        'config' => array(
-            'type' => 'input',
-            'size' => 30,
-            'eval' => 'nospace,alphanum_x,lower'
-        ),
-    ),
-);
+    $newPageColumns = [
+        'style_id' => [
+            'exclude' => 0,
+            'label' => 'LLL:EXT:sitepackageone/Resources/Private/Language/locallang.xlf:style_id',
+            'config' => [
+                'type' => 'input',
+                'size' => 30,
+                'eval' => 'nospace,alphanum_x,lower,unique',
+            ],
+        ],
+        'style_class' => [
+            'exclude' => 0,
+            'label' => 'LLL:EXT:sitepackageone/Resources/Private/Language/locallang.xlf:style_class',
+            'config' => [
+                'type' => 'input',
+                'size' => 30,
+                'eval' => 'nospace,alphanum_x,lower',
+            ],
+        ],
+    ];
 
-ExtensionManagementUtility::addTCAcolumns('pages', $newPageColumns);
+    ExtensionManagementUtility::addTCAcolumns('pages', $newPageColumns);
 
-$GLOBALS['TCA']['pages']['palettes']['sitepackageone'] = array(
-    'canNotCollapse' => 1,
-    'showitem' => 'style_id, style_class'
-);
+    $GLOBALS['TCA']['pages']['palettes']['sitepackageone'] = [
+        'canNotCollapse' => 1,
+        'showitem' => 'style_id, style_class',
+    ];
 
-ExtensionManagementUtility::addToAllTCAtypes(
-    'pages',
-    '--palette--;LLL:EXT:sitepackageone/Resources/Private/Language/locallang.xlf:palettename;sitepackageone,',
-    '',
-    'after:layout'
-);
+    ExtensionManagementUtility::addToAllTCAtypes(
+        'pages',
+        '--palette--;LLL:EXT:sitepackageone/Resources/Private/Language/locallang.xlf:palettename;sitepackageone,',
+        '',
+        'after:layout'
+    );
+
+    // SAME as registered in ext_tables.php
+    $sectionDocType = 369;
+    $sectionDocTypeIconClass = 'tx-sitepackageone-page-section';
+
+    // Add the new doktype to the page type selector
+    ExtensionManagementUtility::addTcaSelectItem(
+        'pages',
+        'doktype',
+        [
+            'label' => 'LLL:EXT:sitepackageone/Resources/Private/Language/locallang.xlf:section_page_type',
+            'value' => $sectionDocType,
+            'icon' => $sectionDocTypeIconClass,
+            'group' => 'special',
+        ],
+    );
+    // Add the icon to the icon class configuration
+    $GLOBALS['TCA']['pages']['ctrl']['typeicon_classes'][$sectionDocType] = $sectionDocTypeIconClass;
+})();

638fd48f697805a9dff22641bcf76e114ba220d4
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2024-08-07 17:43:28 +0200

    [BUGFIX] Fixed pages TCA overrides

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -15,20 +15,20 @@ ExtensionManagementUtility::registerPageTSConfigFile(
 $newPageColumns = array(
     'style_id' => array(
         'exclude' => 0,
-        'label' => 'LLL:EXT:sitepackageone/Resources/Private/Language/locallang.xlf:ww_css_id',
-        'config' => array(
-            'type' => 'input',
-            'size' => 30,
-            'eval' => 'nospace,alphanum_x,lower,unique,required'
-        ),
-    ),
-    'style_class' => array(
-        'exclude' => 0,
-        'label' => 'LLL:EXT:sitepackageone/Resources/Private/Language/locallang.xlf:ww_css_class',
+        'label' => 'LLL:EXT:sitepackageone/Resources/Private/Language/locallang.xlf:style_id',
         'config' => array(
             'type' => 'input',
             'size' => 30,
             'eval' => 'nospace,alphanum_x,lower,unique'
+        ),
+    ),
+    'style_class' => array(
+        'exclude' => 0,
+        'label' => 'LLL:EXT:sitepackageone/Resources/Private/Language/locallang.xlf:style_class',
+        'config' => array(
+            'type' => 'input',
+            'size' => 30,
+            'eval' => 'nospace,alphanum_x,lower'
         ),
     ),
 );
@@ -37,12 +37,12 @@ ExtensionManagementUtility::addTCAcolumns('pages', $newPageColumns);
 
 $GLOBALS['TCA']['pages']['palettes']['sitepackageone'] = array(
     'canNotCollapse' => 1,
-    'showitem' => 'ww_css_id, ww_css_class'
+    'showitem' => 'style_id, style_class'
 );
 
 ExtensionManagementUtility::addToAllTCAtypes(
     'pages',
-    '--palette--;LLL:EXT:sitepackageone/Resources/Private/Language/locallang.xlf:palettename;wwonepagetemplate,',
+    '--palette--;LLL:EXT:sitepackageone/Resources/Private/Language/locallang.xlf:palettename;sitepackageone,',
     '',
     'after:layout'
 );
